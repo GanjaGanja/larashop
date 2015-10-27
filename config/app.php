3a59e4c71598a54e2d6b65e07a6d464c490d8eab
GanjaGanja <ochenj.gorkyj@users.noreply.github.com>
2015-10-27 19:53:15 +0600

    update settings at 'config/app.php'

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'debug' => env('APP_DEBUG', false),
+    'debug' => env('APP_DEBUG', true),
 
     /*
     |--------------------------------------------------------------------------
@@ -39,7 +39,7 @@ return [
     |
     */
 
-    'timezone' => 'UTC',
+    'timezone' => 'Asia/Almaty',
 
     /*
     |--------------------------------------------------------------------------
@@ -78,7 +78,7 @@ return [
     |
     */
 
-    'key' => env('APP_KEY', 'SomeRandomString'),
+    'key' => env('APP_KEY', 'inesindinemwanawabambuyabakwaka'),
 
     'cipher' => 'AES-256-CBC',
 
